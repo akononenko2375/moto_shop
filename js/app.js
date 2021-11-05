65213e5080907e2837dfbbc6cbf07957c923f598
Aleksandr Kononenko <sawa2375@gmail.com>
2021-11-05 18:56:35 +0200

    cards render

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const url = 'https://opensheet.vercel.app/1r52WWS8-y-hs0PPChtHj_XZgkSPconZCokamfzBS-rY/data';
+
+const cartBox = document.querySelector('.cart-box');
+
+sheetData(url);
+
+async function sheetData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const listHTML = data.map((el) => {
+            return `
+            <div class="card" style="width: 15rem;">
+            <div class="card-img"><img src=${el.Image} class="card-img-top" alt="..."></div>
+            <div class="card-body">
+              <h5 class="card-title">${el.Brand} ${el.Model}</h5>
+              <p class="card-text">${shortDescription(el.Description)}</p>
+              <a href="#" class="btn btn-primary">Open</a>
+            </div>
+          </div>
+            `;
+        });
+        for (const item of listHTML) {
+            console.log(item);
+            cartBox.insertAdjacentHTML('beforeend', item);
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+function shortDescription(str) {
+    return `${str.slice(0, 100)}...`;
+}
